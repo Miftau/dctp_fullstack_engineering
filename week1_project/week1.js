3663c4d953d4baa2051016a1ef76163affb61f16
Miftau <miftaudeenb@gmail.com>
2023-12-18 20:41:13 +0100

    fixing return issue

--- week1_project/week1.js
+++ week1_project/week1.js
@@ -1,37 +1,49 @@
 function getUserDetails(){
     let username = prompt("Enter your username")
 while(validateUsername(username) == false){
+    if (username == null){
+        return
+    }
     username = prompt("The username must not be more than 10 characters, please enter a valid username")
+    
+    return
 }
 
 
 let password = prompt("Enter your password")
 while(validatePassword(password) == false){
-    password = prompt("The password must be more than 6 characters, please enter a valid password")
 
-    if (password == username) {
-        password = prompt("You cannot username as a password, enter a valid password")
+    if (password == null){
+        return
+    }
+    
+    else if(password.length < 6){
+        password = prompt("The password must be more than 6 characters, please enter a valid password")
+    }
+
+    else if (password == username) {
+        password = prompt("You cannot use username as a password, enter a valid password")
     }
 }
-
+return
 }
 
 
 function validateUsername(username){
-    if (username == false){
-        return false
+    if (username == null){
+        return 
     }
 
     else if (username.length > 10){
         return false
     } else{
-        return true
+        return 
     }
 }
 
 function validatePassword(password){
     if (password == null){
-        return false
+        return 
     }
 
     else if (password.length < 6 || password == username){
@@ -41,4 +53,6 @@ function validatePassword(password){
     }
 
 }
+
+getUserDetails()
 
